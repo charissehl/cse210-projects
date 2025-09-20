31f2dd72cbb462ff334489dc6e23067353fb853f
charissehl <hea24004@byui.edu>
2025-09-21 08:31:38 +1200

    Completed Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,51 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}. ");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed the course!");
+        }
+
+        else
+        {
+            Console.WriteLine("You have not passed the course this time, but you are able to retake the course and try again next time.");
+        }
+
     }
 }
