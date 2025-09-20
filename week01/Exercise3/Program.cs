8947ff69a90d7bb153710725e0738a87f685fdda
charissehl <hea24004@byui.edu>
2025-09-21 09:22:29 +1200

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Console.Write("What is the magic number? ");
+
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Console.Write("What is your guess? ");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guessNumber = -1;
+
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } 
+
+
+
+
     }
 }
